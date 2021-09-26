c613a18aee9e82c576edb5906205d502d76e0b65
Jonathan Heugh <68595133+Jontas77@users.noreply.github.com>
2021-09-26 22:40:54 +0200

    Added files to project

---
+++ client/src/pages/dashboards/dashComponents/AddProject.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddProject = () => {
+	return <><h1>New Project!</h1></>;
+};
+
+export default AddProject;
---
+++ client/src/pages/dashboards/dashComponents/HeaderDash.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../../stellenbosch-university-logo.png";
+import "../Dashboard.css";
+
+const HeaderDash = ({ logout }) => {
+	return (
+		<header className="header-dashboard">
+			<div className="logo">
+				<img src={logo} className="logo" alt="Stellies Logo" />
+			</div>
+
+			<div className="nav-links">
+				<button className="btn logout-btn" onClick={(e) => logout(e)}>
+					Logout
+				</button>
+			</div>
+		</header>
+	);
+};
+
+export default HeaderDash;
