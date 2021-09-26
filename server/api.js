ba1cf5ad13aa45d5e2f8d2b247da1c15e43b3d13
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-09-26 14:53:34 +0200

    route create project

--- server/api.js
+++ server/api.js
@@ -6,4 +6,14 @@ router.get("/", (_, res) => {
 	res.json({ message: "Welcome to Stellenbosch University" });
 });
 
+router.post("/project", async (req, res) => {
+	try {
+		const { project_name, project_target_group, project_description } = req.body;
+		const newProject = await pool.query('INSERT INTO projects (project_name, project_target_group, project_description) VALUES ($1,$2,$3) RETURNING *', [project_name, project_target_group, project_description]);
+		res.json({ projects: newProject });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+});
+
 export default router;
