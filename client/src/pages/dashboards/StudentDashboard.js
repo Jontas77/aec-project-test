7b7598cf3f45febb7672403aeeed942a7c180586
Jonathan Heugh <68595133+Jontas77@users.noreply.github.com>
2021-09-26 22:30:44 +0200

    Changed styling and added functionality

--- client/src/pages/dashboards/StudentDashboard.js
+++ client/src/pages/dashboards/StudentDashboard.js
@@ -1,12 +1,83 @@
-import React from "react";
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState, useEffect } from "react";
+import HeaderDash from "./dashComponents/HeaderDash";
+import { toast } from "react-toastify";
+import "./Dashboard.css";
+
+import Profile from "./dashComponents/Profile";
+import Projects from "./dashComponents/Projects";
+import Competitions from "./dashComponents/Compititions";
 
 const StudentDashboard = ({ setAuth }) => {
+	const [name, setName] = useState("");
+	// const [message, setMessage] = useState("--No Feedback to Display--");
+	const [page, setPage] = useState("");
+
+	const getName = async () => {
+		try {
+			const response = await fetch("/auth/student/dashboard", {
+				method: "GET",
+				headers: { token: localStorage.token },
+			});
+
+			const parseRes = await response.json();
+
+			setName(parseRes[0].student_name);
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
+
+	useEffect(() => {
+		getName();
+	}, []);
+
+	const logout = (e) => {
+		e.preventDefault();
+		localStorage.removeItem("token");
+		setAuth(false);
+
+		toast.success("Logged out successfully!");
+	};
+
 	return (
-		<div>
-			<h1>WELCOME STUDENT</h1>
-			<h2>DASHBOARD</h2>
-			<button onClick={() => setAuth(false)}>Log out</button>
-		</div>
+		<>
+			<HeaderDash logout={logout} />
+			<div className="container container-fluid no-padding">
+				<div className="introduction">
+					<h1>Student Dashboard</h1>
+					<h2>Welcome Back {name}</h2>
+				</div>
+				{page === "profile" ? (
+					<Profile />
+				) : page === "projects" ? (
+					<Projects setPage={setPage} />
+				) : page === "competitions" ? (
+					<Competitions setPage={setPage} />
+				) : (
+					<>
+						<div className="display">--No Feedback to Display--</div>
+						<div className="links-wrapper">
+							<div className="links">
+								<div className="profile" onClick={() => setPage("profile")}>
+									<i className="fas fa-user"></i>Profile
+								</div>
+								<div className="projects" onClick={() => setPage("projects")}>
+									<i className="fas fa-project-diagram"></i>Projects
+								</div>
+								<div
+									className="competitions"
+									onClick={() => setPage("competitions")}
+								>
+									<i className="fas fa-trophy"></i>Competitions
+								</div>
+							</div>
+						</div>
+					</>
+				)}
+			</div>
+		</>
 	);
 };
 
