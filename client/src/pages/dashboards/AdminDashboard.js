f211af2e4715e9737f1433ad3f39e8068b41cbdc
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-09-28 09:07:22 +0200

    add project button working

--- client/src/pages/dashboards/AdminDashboard.js
+++ client/src/pages/dashboards/AdminDashboard.js
@@ -26,68 +26,65 @@ const AdminDashboard = ({ setAuth }) => {
 
 	useEffect(() => {
 		getProjects();
+		console.log('cleanup')
 	}, [project]);
 
 	return (
-		<div>
-			<Container style={{ width: "100%" }}
-			>
-				{/* HEADING START*/}
-				<Typography variant="h5" component="h2">
-					Welcome back admin@sun.ac.za
-				</Typography>
-				{/* HEADING END */}
-				<br />
-				<br />
-				<Button
-					onClick={() => setAuth(false)}
-					variant='contained'
-				>
-					Log out
-				</Button>
-				<br />
-				<br />
-				<div>
+			<div>
+				<Container style={{ width: "100%" }}>
+					{/* HEADING START*/}
+					<Typography variant="h5" component="h2">
+						Welcome back admin@sun.ac.za
+					</Typography>
+					{/* HEADING END */}
+					<br />
+					<br />
 					<Button
-						onClick={handleClick}
+						onClick={() => setAuth(false)}
 						variant='contained'
 					>
-						Add project
+						Log out
 					</Button>
+					<br />
+					<br />				
+							<div>
+								<Button
+									onClick={handleClick}
+									variant='contained'
+								>
+									Add project
+								</Button>
 					<div>
-						<br />
-					<div>
-					{project ? (
-						<ProjectA />
-					) : (
-									projects.map(({ project_name, project_description, project_image, project_target_group }) => {
-							return project === "" ? (
-								<h3>--No projects to display--</h3>
-							) : (
-									<div key={project_name}>
-										<div className="card mb-3" >
-											<div className="row g-0">
-												<div className="col-md-4">
-													<img src={project_image} className="img-fluid rounded-start" alt="..." />
-												</div>
-												<div className="col-md-8">
-													<div className="card-body">
-														<h5 className="card-title">{project_name} created by {project_target_group}</h5>
-														<p className="card-text">{project_description}</p>
-														<p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+						{project && <ProjectA />}
+					</div>
+									<br />
+									<div>
+										{projects.map(({ project_name, project_description, project_image, project_target_group }) => {
+											return project === "" ? (
+												<h3>--No projects to display--</h3>
+											) : (
+												<div key={project_name}>
+													<div className="card mb-3" >
+														<div className="row g-0">
+															<div className="col-md-4">
+																<img src={project_image} className="img-fluid rounded-start" alt="..." />
+															</div>
+															<div className="col-md-8">
+																<div className="card-body">
+																	<h5 className="card-title">{project_name} created by {project_target_group}</h5>
+																	<p className="card-text">{project_description}</p>
+																	<p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+																</div>
+															</div>
+														</div>
 													</div>
 												</div>
-											</div>
-										</div>
+											);
+										})}										
+									</div>
 								</div>
-							);
-						})
-						)}
-					</div>
-				</div>
-				</div>
-			</Container>
-		</div>
+				</Container>
+			</div>
 	);
 };
 
