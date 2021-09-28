4d0d38785ecd4f4b889597d0f75a6e79d2aae853
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-09-28 09:38:44 +0200

    new component

---
+++ client/src/pages/projects/AllProjects.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const AllProjects = ({projects, allProject}) => {
+  return (
+    <div>
+      {projects.map(({ project_name, project_description, project_image, project_target_group }) => {
+        return allProject === "" ? (
+          <h3>--No projects to display--</h3>
+        ) : (
+          <div key={project_name}>
+            <div className="card mb-3" >
+              <div className="row g-0">
+                <div className="col-md-4">
+                  <img src={project_image} className="img-fluid rounded-start" alt="..." />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">{project_name} created by {project_target_group}</h5>
+                    <p className="card-text">{project_description}</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  )
+}
+
+export default AllProjects
