39985ccc46141b564e5d67e992b90f2b158de3fd
Jonathan Heugh <mrjbheugh@gmail.com>
2021-11-19 15:12:10 +0200

    modified app.use path

--- server/app.js
+++ server/app.js
@@ -21,8 +21,8 @@ const staticDir = path.join(__dirname, "static");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use("/api/image", express.static("uploads"));
+app.use(express.urlencoded({ extended: true }));
+app.use("/", express.static(path.join(__dirname, "/")));
 app.use(configuredHelmet());
 app.use(morgan("dev"));
 app.use(cors());
